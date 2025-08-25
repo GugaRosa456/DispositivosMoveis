e58c008b6e6bc7f47f68595c4f9f911651b22eb2
GugaRosa456 <rosaguga07@gmail.com>
2025-08-25 09:16:47 -0300

    sorteio v1

--- app/src/main/java/com/example/minhaaplicao/MainActivity.java
+++ app/src/main/java/com/example/minhaaplicao/MainActivity.java
@@ -13,47 +13,11 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     TextView textView;
-    int contador=0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("ciclo de vida", "Oncreate ");
-
-
-        }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d("ciclo_vida", "onStart");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d("ciclo_vida", "onResume");
 
     }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d("ciclo_vida", "onPause");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop(); Log.d("ciclo_vida", "onStop");
-
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d("ciclo_vida", "onDestroy");
-
-    }
-
-
 }
