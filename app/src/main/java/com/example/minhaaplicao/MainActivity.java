d69475852c22701830f5827d1948d82a474782b7
GugaRosa456 <rosaguga07@gmail.com>
2025-09-01 08:56:48 -0300

    v2 Sorteio

--- app/src/main/java/com/example/minhaaplicao/MainActivity.java
+++ app/src/main/java/com/example/minhaaplicao/MainActivity.java
@@ -1,23 +1,36 @@
 package com.example.minhaaplicao;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    TextView textView;
+    Button button;
+    EditText editTextMin, editTextMax;
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button =findViewById(R.id.button);
+        editTextMin =findViewById(R.id.editMin);
+        editTextMax =findViewById(R.id.editMax);
+        tv = findViewById(R.id.tvResultado);
 
+        button.setOnClickListener(v -> {
+            Random random= new Random();
+            int min, max;
+            min=Integer.parseInt(editTextMin.getText().toString());
+            max=Integer.parseInt(editTextMax.getText().toString());
+
+            int sortiado = random.nextInt();
+            tv.setText(Integer.toString(sortiado));
+        });
     }
 }
